0ca2b3e086ff9406441e7acc4f0fd06dda9a0d1c
whitebuless <fan727gold@outlook.com>
2024-02-21 14:54:40 +0800

    http

--- http/httStart.js
+++ http/httStart.js
@@ -3,10 +3,15 @@ const http=require('http')
 
 // 创建服务对象
 const server=http.createServer((request,response)=>{
-  response.end('hello http server');//设置响应体
+  //设置响应头，告诉浏览器采用html格式，编码字符集为utf-8防止中文乱码问题
+  response.setHeader('content-type','text/html;charset=utf-8')
+  response.end('<h1>你好<h1>');//设置响应体
 });
 
 // 监听端口，启动服务,
 server.listen(9000,()=>{
   console.log('服务已经启动');
 })
+
+
+// 
