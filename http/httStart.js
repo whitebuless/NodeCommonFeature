d9f00b7b5c1311bdfeba55a5102f527356a1025e
whitebuless <fan727gold@outlook.com>
2024-02-22 15:45:45 +0800

    http

--- http/httStart.js
+++ http/httStart.js
@@ -5,13 +5,10 @@ const http=require('http')
 const server=http.createServer((request,response)=>{
   //设置响应头，告诉浏览器采用html格式，编码字符集为utf-8防止中文乱码问题
   response.setHeader('content-type','text/html;charset=utf-8')
-  response.end('<h1>你好<h1>');//设置响应体
+  response.end('<h1>你好</h1><b>2</b>');//设置响应体
 });
 
 // 监听端口，启动服务,注意端口不要被占用
 server.listen(9000,()=>{
   console.log('服务已经启动');
 })
-
-
-// 
